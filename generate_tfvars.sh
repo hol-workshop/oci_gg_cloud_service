f2022b1c9cd2846d0e31b9c3065783c6fb4b58a4
hol-workshop <74899512+hol-workshop@users.noreply.github.com>
2021-08-20 15:57:12 +0300

    Create generate_tfvars.sh

---
+++ generate_tfvars.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+var_tenancy_ocid=$( oci iam compartment list --access-level ACCESSIBLE |\
+grep -i tenancy | \
+awk 'NR==1{print $2}' | \
+sed -e 's/,//g' -e 's/"//g')
+
+var_region=$(printenv REGION)
+
+function check_tenancy() {
+ echo "tenancy_ocid = \"$var_tenancy_ocid\"" >> variables.tf
+}
+
+function check_compartment() {
+ echo "compartment_ocid = \"$var_tenancy_ocid\"" >> variables.tf
+}
+
+function check_region() {
+echo "region = \"$var_region\"" >> variables.tf
+}
+
+check_tenancy
+check_region
+check_compartment
